82d7ae90a6ead3cdf6254bb08995a9259d744199
thomasdunlap <tom.a.dunlap@gmail.com>
2018-08-10 20:57:08 -0400

    feat: started working on GaussinNB::training() in gaussiannb.cpp.

--- src/gaussiannb.cpp
+++ src/gaussiannb.cpp
@@ -1,4 +1,4 @@
-#include "gaussian.h"
+#include "gaussiannb.h"
 #include <vector>
 #include <sstream>
 #include <fstream>
@@ -7,3 +7,24 @@
 #include <iostream>
 
 using namespace std;
+
+
+GaussianNB::GaussianNB() {
+
+}
+
+GaussianNB::~GaussianNB() {}
+
+void GaussianNB::training(std::vector<std::vector<double>> data, std::vector<string> labels) {
+  std::vector<std::vector<std::vector<double>>> data_s;
+
+  for (int i=0; i < 3; i++) {
+    std::vector<std::vector<double>> class_data;
+    data_s.push_back(class_data);
+
+    for (int j=0; j<4; j++) {
+      std::vector<double> observed;
+      data_s[i].push_back(observed);
+    }
+  }
+}
