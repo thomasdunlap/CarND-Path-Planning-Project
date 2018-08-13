777dc47e5831ebe23ac97d358b5172fd78f41652
thomasdunlap <tom.a.dunlap@gmail.com>
2018-08-12 23:53:28 -0400

    fix: addressed uneven brackets in GaussianNB::trainig.

--- src/gaussiannb.cpp
+++ src/gaussiannb.cpp
@@ -35,28 +35,29 @@ void GaussianNB::training(std::vector<std::vector<double>> data, std::vector<str
     } else if (labels[i] == "center") {
       class_ = center;
     }
+
+
+    data_s[class_][s].push_back(data[i][s]);
+    data_s[class_][d].push_back(fmod(data[i][d], 4.0));
+    data_s[class_][s_dot].push_back(data[i][s_dot]);
+    data_s[class_][d_dot].push_back(data[i][d_dot]);
   }
 
-  data_s[class_][s].push_back(data[i][s]);
-  data_s[class_][d].push_back(fmod(data[i][d], 4.0));
-  data_s[class_][s_dot].push_back(data[i][s_dot]);
-  data_s[class_][d_dot].push_back(data[i][d_dot]);
+  std::vector<std::vector<double>> averages_;
+  std::vector<std::vector<double>> sigs_;
+
+  for (int i=0; i<3; i++) {
+      std::vector<double> row_average;
+      std::vector<double> row_sigs;
+
+      for (int j=0; j<4; j++) {
+        row_average.push_back(average(data_s[i][j]));
+        row_sigs.push_back(sigs(data_s[i][j]));
+      }
+      averages_.push_back(row_average);
+      sigs_.push_back(row_sigs);
+  }
+
+  averages_= averages_;
+  sigs_ = sigs_;
 }
-
-std::vector<std::vector<double>> averages_;
-std::vector<std::vector<double>> sigs_;
-
-for (int i=0; i<3; i++) {
-    std::vector<double> row_average;
-    std::vector<double> row_sigs;
-
-    for (int j=0; j<4; j++) {
-      row_average.push_back(average(data_s[i][j]));
-      row_sigs.push_back(sigs(data_s[i][j]));
-    }
-    averages_.push_back(row_average);
-    sigs_.push_back(row_sigs);
-}
-
-averages_= averages_;
-sigs_ = sigs_;
