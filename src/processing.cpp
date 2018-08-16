305fd283e49e35c42c4478de16e4e5774559ff8f
thomasdunlap <tom.a.dunlap@gmail.com>
2018-08-15 23:55:24 -0400

    feat: completed first pass at read_state() in processing.cpp.

--- src/processing.cpp
+++ src/processing.cpp
@@ -4,3 +4,25 @@
 #include <fstream>
 #include <math.h>
 #include <vector>
+
+using namespace std;
+
+std::vector<std::vector<double>> read_state(string path) {
+  ifstream state_in(path.c_str(), ifstream::in);
+  std::vector<std::vector<double>> states;
+  string line;
+
+  while (getline(state_in, line)) {
+    istringstream iss(line);
+    std::vector<double> x_pos;
+
+    string token;
+
+    while (getline(iss, token, ',')) {
+      x_pos.push_back(stod(token));
+    }
+    states.push_back(x_pos);
+  }
+
+  return states;
+}
